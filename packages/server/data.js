329a5bc776e9777bd6926303216141b1b7425e4c
kirilinsky <kirill.ilinsky@gmail.com>
2022-12-13 13:30:24 +0100

    add mock

---
+++ packages/server/data.js
@@ -0,0 +1,16 @@
+let data = [
+    {
+        id: '62163307-03aa-40f9-96af-8a89c4905af0',
+        name: 'Jeans FIT',
+        color: 'denim',
+        image: 'https://static.zara.net/photos///2022/I/0/2/p/8727/410/712/2/w/1126/8727410712_6_1_1.jpg?ts=1657619428038'
+    },
+    {
+        id: "013424f0-4ea3-451e-be32-76a1fe998d4d",
+        name: 'Linen Shirt',
+        color: "white",
+        image: "https://static.zara.net/photos///2022/V/0/2/p/1063/407/250/2/w/1126/1063407250_6_1_1.jpg?ts=1642163706912"
+    }
+]
+
+module.exports = { data }
--- packages/server/index.js
+++ packages/server/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 
 const port = process.env.PORT || 5001
 
+const data = require('./data')
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('backend live status')
+})
+
+app.get('/items', (req, res) => {
+    res.send(data)
 })
 
 app.listen(port, (e) => {
